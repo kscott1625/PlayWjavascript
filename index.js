b6a5326f22b638e4e3660dba857ef7d7431539c2
Katherine Scott <kasc1625@gmail.com>
2022-08-19 11:42:30 -0700

    modified:   index.js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 let firstCard = 7;
 let secondCard = 9;
+let cards = [];
 let hasBlackjack = false;
 let isAlive = true;
 let message = '';
 
 let messageEl = document.getElementById('message-el');
-console.log(messageEl);
+
+let sumEl = document.getElementById('sum-el');
+console.log(sumEl);
+
+let cardsEl = document.getElementById('cards-el');
+console.log(cardsEl);
 
 let sum = firstCard + secondCard;
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  sumEl.textContent = 'Sum: ' + sum;
+  cardsEl.textContent = 'Cards: ' + cards[0] + '  ' + cards[1];
   if (sum <= 20) {
     message = 'Another card?';
   } else if (sum === 21) {
@@ -20,4 +32,11 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log('new card');
+  let card = 4;
+  sum += card;
+  renderGame();
 }
