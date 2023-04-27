a93661a57ae48689db161f56b533125d34d611bb
Katherine Scott <kasc1625@gmail.com>
2023-04-26 17:25:59 -0700

    modified:   index.css 	modified:   index.html 	modified:   index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const trigger = document.querySelector('#firstBtn');
+const modalWrapper = document.querySelector('.modal_wrapper');
+const closeBtn = document.querySelector('.close');
+
+trigger.addEventListener('click', function(){
+    openModal();
+});
+
+closeBtn.addEventListener('click', function(){
+    closeModal();
+});
+
+modalWrapper.addEventListener('click', function(e){
+    if(e.target !== this) return;
+    closeModal();
+});
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape') {
+        closeModal();
+    }
+})
+
+function openModal() {
+    modalWrapper.classList.add('active');
+}
+function closeModal() {
+    modalWrapper.classList.remove('active');
+}
